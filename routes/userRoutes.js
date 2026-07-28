dcff883b5b3da274a7bc306a50499dd8644d903a
Kingsley Ivom <148369288+legacy-king@users.noreply.github.com>
2026-07-28 23:59:16 +0100

    Add root route redirect to login

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -7,5 +7,8 @@ router.post('/login', userController.login);
 router.get('/dashboard', userController.dashboard);
 router.get('/login', userController.loginPage);
 router.get('/signup', userController.signupPage);
+router.get('/', (req, res) => {
+  res.redirect('/login');
+});
 
 module.exports = router;
